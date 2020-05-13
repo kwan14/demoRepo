df5498f3d721ebd4cd0d23d99ac1d1222fb06460
Mendel Kwan <mendel.kwan@telus.net>
2020-05-13 10:45:22 -0700

    updated second matrix

--- sampleScript.py
+++ sampleScript.py
@@ -5,7 +5,7 @@ my_matrix = np.eye(4)
 my_second_matrix = np.array[[1, 2, 3, 4],
                 [2, 3, 4, 5],
                 [4, 0, 9, 8],
-                [2, 3, 6, 6]]
+                [2, 3, 6, 1]]
 
 print(my_matrix)
 print(my_second_matrix)
