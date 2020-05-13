4619aa493ec2919ffef1dde00945037a32eb0ba4
Mendel Kwan <mendel.kwan@telus.net>
2020-05-13 10:41:21 -0700

    added sample

---
+++ sampleScript.py
@@ -0,0 +1,11 @@
+import numpy as numpy
+import pandas as pandas
+
+my_matrix = np.eye(4)
+my_second_matrix = np.array[[1, 2, 3, 4],
+                [2, 3, 4, 5],
+                [4, 0, 9, 8],
+                [2, 3, 6, 6]]
+
+print(my_matrix)
+print(my_second_matrix)
